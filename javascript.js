49b5881fccf39b29aa3a4fd0841ef36ee874ff00
kb419940 <s102321044@gmail.com>
2023-01-20 17:22:38 +1100

    add getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3);   //generate 0~2
+    let rockpapperscissorArray = ["Rock","Paper","Scissors"];
+
+    return rockpapperscissorArray[randomNumber];
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(getComputerChoice());
+}
