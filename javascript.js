6d3b4b16340b37f286c3302675db2b412d1073ac
kb419940 <s102321044@gmail.com>
2023-02-28 21:20:54 +1100

    create 3 button, add eventListener('click'), next : write show Score function

--- javascript.js
+++ javascript.js
@@ -26,26 +26,74 @@ function playCompare(userChoice, computerChoice){
 
 function playRound(playerSelection, computerSelection) {
 
-    let result = ``;
+    let result = [];
 
     if (playCompare(playerSelection, computerSelection) === "draw"){
-        result = `Battle to a draw!`;
+        result[0] = `Battle to a draw!`;
+        result[1] = 0;
     } else if (playCompare(playerSelection, computerSelection) === "playerWin"){
-        result = `You Win! ${playerSelection}(you) beats ${computerSelection}(pc)`;
+        result[0] = `You Win! ${playerSelection}(you) beats ${computerSelection}(pc)`;
+        result[1] = 1;
     }else{
-        result = `You Lose! ${computerSelection}(pc) beats ${playerSelection}(you)`;
+        result[0] = `You Lose! ${computerSelection}(pc) beats ${playerSelection}(you)`;
+        result[1] = -1;
     }
     return result;
-  }
-  
-  function game(playerSelection){
-    for (let i = 1; i <= 5; i++) {
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));      
-    }
-  }
+}
+
+function showScore(singleResult, player_win, draw, player_lose) {
+    
+}
+
+const buttons = document.querySelectorAll('button');
+const resultContainer = document.querySelector('.result');
+const resultContent = document.createElement('h2');
+resultContent.style.cssText = 'text-align: center;';
+const runningScore = document.createElement('h3');
+
+let player_win = 0;
+let draw = 0;
+let pc_win = 0;
+
+buttons.forEach(button => {
+    
+    button.addEventListener('click', ()=>{
+        let playerSelection = button.id;
+        let resultArray = playRound(playerSelection, getComputerChoice());
+
+        resultContent.classList.add('content');
+        resultContent.textContent = resultArray[0];
+        resultContainer.appendChild(resultContent);
+        switch (resultArray[1]) {
+            case -1:
+                pc_win += 1; 
+                break;
+            case 0:
+                draw += 1; 
+                break;
+            case 1:
+                player_win += 1;
+                break;
+            default:
+                break;
+        }
+
+        console.log(`player_win = ${player_win}, draw = ${draw}, pc_win = ${pc_win}`);
+    })
+});
 
 
-  const playerSelection = "rock";
-  game(playerSelection);
+
+
+//   function game(playerSelection){
+//     for (let i = 1; i <= 5; i++) {
+//         let playerSelection = prompt(`round ${i} : rock, papper, scissor?`);
+//         const computerSelection = getComputerChoice();
+//         console.log(playRound(playerSelection, computerSelection));      
+//     }
+//   }
+
+
+//   const playerSelection = "rock";
+//   game(playerSelection);
 
